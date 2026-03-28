c9a2480ca97afb5bd4c0cc35770eebf4397380d4
CoderAalok <yadavaalok565@gmail.com>
2026-03-28 15:06:09 +0545

    linkedList_cycle.py

--- Fast and Slow Technique/linkedList_cycle.py
+++ Fast and Slow Technique/linkedList_cycle.py
@@ -10,27 +10,25 @@ class Solution:
         fast = head
         while fast and fast.next:
             slow = slow.next
-            fast = fast.next.next.next
+            fast = fast.next.next
             
             if slow == fast:
-                return True 
+                return True
         
         return False
 
 # these all are contains a container itself
 node1 = ListNode(3)
 node2 = ListNode(2)
-# node3 = ListNode(-1)
-# node4 = ListNode(0)
+node3 = ListNode(-1)
+node4 = ListNode(0)
 
 #connecting each node together
-# [node1] -> [node2] -> [node3] -> [node4] -> [node3]
+# [node1] -> [node2] -> [node3] -> [node4] -> [node3]l
 node1.next = node2
 node2.next = node2
-# node3.next = node4
-# node4.next = node3
-
-
+node3.next = node4
+node4.next = node3
 
 s = Solution()
 print(s.hasCycle(node1))
