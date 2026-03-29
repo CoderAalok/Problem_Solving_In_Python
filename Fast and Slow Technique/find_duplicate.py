47f70319032c938a8af9d81321e0ccbab466bde4
CoderAalok <yadavaalok565@gmail.com>
2026-03-29 18:16:31 +0545

    Find Duplicate

--- Fast and Slow Technique/find_duplicate.py
+++ Fast and Slow Technique/find_duplicate.py
@@ -6,10 +6,8 @@
 #             return n
 #         seen.add(n)
         
-
-
-# Optimal approach
-from typing import List, Optional
+# Second approach
+from typing import List
 # class Solution:
 #     def findDuplicate(self, nums:List[int]) -> int:
 #         slow = fast = nums[0] 
@@ -29,67 +27,28 @@ from typing import List, Optional
                 
 #                 return slow
 
-
-# nums = [1,2,3,4,2]
 # s = Solution()
 # print(s.findDuplicate(nums))
-
-
-# class Solution:
-#     def find_duplicate(self, nums:List[int]) -> int:
-#         slow = nums[0]
-#         fast = nums[0]
-#         # step-1: cycle detection
-#         while True:
-#             slow = nums[slow]
-#             fast = nums[nums[fast]]
-#             if slow == fast:
-#                 break        
-
-#         slow = nums[0]  #resetting 
-#         # step-2: entry point of cycle
-#         while slow != fast:
-#             slow = nums[slow]
-#             fast = nums[fast]
-        
-#         # duplicate found
-#         return slow
-
-# s = Solution()
-# print(s.find_duplicate([1,2,3,4,3]))
-
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# Optimal approach
 class Solution:
-    def isPalindrome(self, head: Optional[ListNode]):
-        n = ""
-        slow = head
-        while slow:
-            n += str(slow.val)
-            slow = slow.next
-        
-        n = int(n)
-        x = n
-        rem = 0
-        while n != 0:
-            rem = (rem*10) + (n % 10)
-            n //= 10
-        
-        if rem == x:
-            return True
-        return False
+    def find_duplicate(self, nums:List[int]) -> int:
+        slow = nums[0]
+        fast = nums[0]
+        # step-1: cycle detection
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break        
 
-n1 = ListNode(1)
-n2 = ListNode(2)
-n3 = ListNode(2)
-n4 = ListNode(1)
-
-n1.next= n2
-n2.next = n3
-n3.next = n4
+        slow = nums[0]  #resetting 
+        # step-2: entry point of cycle
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        
+        # duplicate found
+        return slow
 
 s = Solution()
-print(s.isPalindrome(n1))
+print(s.find_duplicate([1,2,3,4,3]))
