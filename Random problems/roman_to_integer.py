7a2239ff5a5326fec6804da1d0e4455fc3aa9f07
CoderAalok <yadavaalok565@gmail.com>
2026-06-20 07:26:05 +0545

    Add roman to integer conversion function

---
+++ Random problems/roman_to_integer.py
@@ -0,0 +1,27 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_nums = {
+            "M": 1000,
+            "D": 500,
+            "C": 100,
+            "L":  50,
+            "X": 10,
+            "V": 5,
+            "I": 1
+        }
+
+        prev = 0
+        ans = 0
+        for i in range(len(s)):
+            curr = roman_nums[s[i]]
+            if curr > prev:
+                ans  = ans - 2 * prev + curr
+            else:
+                ans += curr
+            
+            prev = curr
+
+        return ans
+        
+s = Solution()
+print(s.romanToInt('MMCDX'))  
