f18f45a26810c8729d1fa27c1035e0e1d9ee7ac8
CoderAalok <yadavaalok565@gmail.com>
2026-08-09 00:39:42 +0545

    Fix LCM function to handle zero and negative inputs correctly; update test values

--- Random problems/Calculate_LCM.py
+++ Random problems/Calculate_LCM.py
@@ -1,7 +1,7 @@
 def LCM(a: int, b: int) -> int:
     """To calculate Least Common Multiple(LCM): Multiply a and b then divide by HCF(or GCF)."""
     # check negative
-    if a < 0 or b < 0:
+    if a <= 0 or b <= 0:
         return 0
         
     # calculate HCF
@@ -15,5 +15,5 @@ def LCM(a: int, b: int) -> int:
     return lcm
 
 # test
-a, b = 60, 90
+a, b = 90, 100
 print(LCM(a, b))
